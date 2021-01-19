925d2ca9124f0104af5f18468f8fc7d94bd54f4c
TiagoSRangel <71654008+TiagoSRangel@users.noreply.github.com>
2021-01-19 17:17:34 +0000

    Add files via upload

---
+++ Cartas Yugioh/CartasYugioh.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Cartas_Yugioh
+{
+    public class CartasYugioh
+    {
+        public int id { get; set; }
+        public int atk { get; set; }
+        public int def { get; set; }
+        public int level { get; set; }
+        public string name { get; set; }
+        public string type { get; set; }
+        public string desc { get; set; }
+    }
+}
